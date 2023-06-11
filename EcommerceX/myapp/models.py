aaed68e30bd34202b55586f04829007696e092d6
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2023-06-11 07:58:44 +0530

    video completed upto 1:32 and i have to add data into database

--- EcommerceX/myapp/models.py
+++ EcommerceX/myapp/models.py
@@ -3,12 +3,13 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 STATE_CHOICES = (
-    ('Uttar pradesh', 'Uttar pradesh'),
-    ('Haryana', 'Haryana'),
-    ('Goa', 'Goa'),
-    ('Madhya pradesh', 'Madhya pradesh'),
-    ('Rajasthan', 'Rajastand')
+    ("Uttar pradesh", "Uttar pradesh"),
+    ("Haryana", "Haryana"),
+    ("Goa", "Goa"),
+    ("Madhya pradesh", "Madhya pradesh"),
+    ("Rajasthan", "Rajastand"),
 )
+
 
 class Customer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -20,11 +21,10 @@ class Customer(models.Model):
 
     def __str__(self):
         return str(self.id)
-    
-CATEGORY_CHOICES = (
-    ('M', 'Mobiles'),
-    ('L', 'Laptops')
-)
+
+
+CATEGORY_CHOICES = (("M", "Mobiles"), ("L", "Laptops"))
+
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
@@ -33,10 +33,11 @@ class Product(models.Model):
     description = models.TextField()
     brand = models.CharField(max_length=100)
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
-    product_image = models.ImageField(upload_to='productimg')
+    product_image = models.ImageField(upload_to="productimg")
 
     def __str__(self):
         return str(self.id)
+
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -44,15 +45,17 @@ class Cart(models.Model):
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
-        return str(self.id) 
-    
-STATUS_CHOICES =(
-    ('Accepted', 'Accepted'),
-    ('Packed', 'Packed'),
-    ('On the way', 'On the way'),
-    ('Delivered', 'Delivered'),
-    ('Cancel', 'Cancel')
+        return str(self.id)
+
+
+STATUS_CHOICES = (
+    ("Accepted", "Accepted"),
+    ("Packed", "Packed"),
+    ("On the way", "On the way"),
+    ("Delivered", "Delivered"),
+    ("Cancel", "Cancel"),
 )
+
 
 class OrderPlaced(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -60,4 +63,4 @@ class OrderPlaced(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     ordered_date = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Pending')
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="Pending")
