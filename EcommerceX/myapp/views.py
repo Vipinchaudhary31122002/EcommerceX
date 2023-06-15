03293b723c9f1c65d9935fb05ab0a2d36eed5f87
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2023-06-15 16:00:28 +0530

    made some changes

--- EcommerceX/myapp/views.py
+++ EcommerceX/myapp/views.py
@@ -1,37 +1,54 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Customer, Product, Cart, OrderPlaced
 
-def home(request):
- return render(request, 'app/home.html')
+# class of product 
+class ProductView(View):
+    def get(self, request):
+        mobiles = Product.objects.filter(category='M')
+        laptops = Product.objects.filter(category='L')
+        return render(request, 'home.html', {'mobiles':mobiles, 'laptops':laptops})
 
-def product_detail(request):
- return render(request, 'app/productdetail.html')
+class ProductDetailView(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, 'productdetail.html', {'product':product})
 
 def add_to_cart(request):
- return render(request, 'app/addtocart.html')
+    return render(request, "addtocart.html")
+
 
 def buy_now(request):
- return render(request, 'app/buynow.html')
+    return render(request, "buynow.html")
+
 
 def profile(request):
- return render(request, 'app/profile.html')
+    return render(request, "profile.html")
+
 
 def address(request):
- return render(request, 'app/address.html')
+    return render(request, "address.html")
+
 
 def orders(request):
- return render(request, 'app/orders.html')
+    return render(request, "orders.html")
+
 
 def change_password(request):
- return render(request, 'app/changepassword.html')
+    return render(request, "changepassword.html")
+
 
 def mobile(request):
- return render(request, 'app/mobile.html')
+    return render(request, "mobile.html")
+
 
 def login(request):
- return render(request, 'app/login.html')
+    return render(request, "login.html")
+
 
 def customerregistration(request):
- return render(request, 'app/customerregistration.html')
+    return render(request, "customerregistration.html")
+
 
 def checkout(request):
- return render(request, 'app/checkout.html')
+    return render(request, "checkout.html")
