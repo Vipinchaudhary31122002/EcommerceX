0b5cc8886abed9716b86dc4d0f1199566b97562f
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2023-06-17 11:26:37 +0530

    video completed 2:36 and now i have to work on login page

--- EcommerceX/myapp/urls.py
+++ EcommerceX/myapp/urls.py
@@ -8,15 +8,15 @@ urlpatterns = [
     path("", views.ProductView.as_view(), name="home"),
     # url of product detail-page
     path("product-detail/<int:pk>", views.ProductDetailView.as_view(), name="product-detail"),
-
+    # url of customerregistration page 
+    path("registration/", views.CustomerRegistrationView.as_view(), name="customerregistration"),
+    
     path("cart/", views.add_to_cart, name="add-to-cart"),
     path("buy/", views.buy_now, name="buy-now"),
     path("profile/", views.profile, name="profile"),
     path("address/", views.address, name="address"),
     path("orders/", views.orders, name="orders"),
     path("changepassword/", views.change_password, name="changepassword"),
-    path("mobile/", views.mobile, name="mobile"),
     path("login/", views.login, name="login"),
-    path("registration/", views.customerregistration, name="customerregistration"),
     path("checkout/", views.checkout, name="checkout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- EcommerceX/myapp/views.py
+++ EcommerceX/myapp/views.py
@@ -1,18 +1,34 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Customer, Product, Cart, OrderPlaced
+from .forms import CustomerRegistrationForm
+from django.contrib import messages
 
-# class of product 
+# view for home page
 class ProductView(View):
     def get(self, request):
         mobiles = Product.objects.filter(category='M')
         laptops = Product.objects.filter(category='L')
         return render(request, 'home.html', {'mobiles':mobiles, 'laptops':laptops})
 
+# view for product detail page
 class ProductDetailView(View):
     def get(self, request, pk):
         product = Product.objects.get(pk=pk)
         return render(request, 'productdetail.html', {'product':product})
+
+# view for customer registration page
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'customerregistration.html', {'form':form})
+
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Congratulations! Registration Successfull')
+            form.save()
+        return render(request, 'customerregistration.html', {'form':form})
 
 def add_to_cart(request):
     return render(request, "addtocart.html")
@@ -37,17 +53,8 @@ def orders(request):
 def change_password(request):
     return render(request, "changepassword.html")
 
-
-def mobile(request):
-    return render(request, "mobile.html")
-
-
 def login(request):
     return render(request, "login.html")
-
-
-def customerregistration(request):
-    return render(request, "customerregistration.html")
 
 
 def checkout(request):
